464b2a89da61fe40ad8ad4aabcb054dcad85e77f
Maddax10 <maximilien01993@gmail.com>
2025-10-24 01:54:49 +0200

    Add routes in docs

--- controllers/docs.routes.js
+++ controllers/docs.routes.js
@@ -3,26 +3,38 @@ import { Router } from 'express';
 const router = Router();
 
 export const routes = [
-  // health
-  { ________________________________: 'health' },
-  { method: 'GET', path: '/health' },
+	// health
+	{ ________________________________: 'health' },
+	{ method: 'GET', path: '/health' },
 
-  // auth & users
-  // users (admin-only list)
-  { ________________________________: 'Auth & users' },
-  {
-    method: 'POST',
-    path: '/login',
-    jsonObject: {
-      email: 'mail',
-      password: 'password',
-    },
-  },
+	// auth & users
+	// users (admin-only list)
+	{ ________________________________: 'Auth & users' },
+	{
+		method: 'POST',
+		path: '/login',
+		jsonObject: {
+			email: 'mail',
+			password: 'password',
+		},
+	},
+	// Projects
+	{ ________________________________: 'Projects' },
+	{
+		method: 'GET',
+		path: '/projects/all',
+	},
+	// Skills
+	{ ________________________________: 'Skills' },
+	{
+		method: 'GET',
+		path: '/skills/all',
+	},
 ];
 
 //A la racine de "/doc"
 router.get('/', (_req, res) => {
-  return res.status(200).json(routes);
+	return res.status(200).json(routes);
 });
 
 export default router;
