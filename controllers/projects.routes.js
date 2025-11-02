ed18742b6122fb89cd3470f24093ae8bc92d9185
Maddax10 <maximilien01993@gmail.com>
2025-11-02 04:44:36 +0100

    add POST PUT DELETE routes => skills
    add POST PUT routes => projects

--- controllers/projects.routes.js
+++ controllers/projects.routes.js
@@ -3,6 +3,32 @@ import db from '../db/database.js';
 import requireAuth from '../middleware/auth.js';
 
 const router = Router();
+const uniqueConstraint = `SQLITE_CONSTRAINT_UNIQUE`;
+const noDataInBDD = `No data in bdd`;
+
+const isValidString = (s) => {
+	return typeof s === 'string' && s.trim().length > 0;
+};
+const isValidNumber = (n) => {
+	return Number.isInteger(n) && n > 0;
+};
+/**
+ * Modifie la structure de l'objet renvoyé pour ne pas matcher avec la bdd
+ * @param {*} rows
+ * @returns
+ */
+const getData = (rows) => {
+	return {
+		title: rows.title_projects,
+		description: rows.description_projects,
+		github: rows.github_projects,
+		image_path: rows.image_path_projects,
+		skills: rows.skills_projects,
+	};
+};
+/**==========================================================
+ * GET
+   ==========================================================*/
 
 router.get('/all', (req, res) => {
 	try {
@@ -17,4 +43,68 @@ router.get('/all', (req, res) => {
 		return res.status(500).json({ error: err.message });
 	}
 });
+/**==========================================================
+ * POST
+   ==========================================================*/
+
+/*
+ * Objet attendu en body : 
+  {
+    "title" : "require",
+    "description" : "require",
+    "github" : "require",
+    "image_path" : "require",
+    "skills" : "require"
+  }
+*/
+router.post('/create', requireAuth, (req, res) => {
+	const { title, description, github, image_path, skills } = req.body;
+	try {
+		if (!title || !description || !github || !image_path || !skills) throw new Error('champ vide ou mauvais objet envoyé');
+		if (!isValidString(title) || !isValidString(description) || !isValidString(github) || !isValidString(image_path) || !isValidString(skills)) throw new Error('champ vide ou mauvais objet envoyé');
+
+		const rows = db.prepare(`INSERT INTO projects (title_projects, description_projects, github_projects, image_path_projects, skills_projects) VALUES (?,?,?,?,?) RETURNING *;`).get(title, description, github, image_path, skills);
+		const data = getData(rows);
+
+		return res.status(200).json(data);
+	} catch (e) {
+		if (e.code === uniqueConstraint) return res.status(401).json({ erreur: 'Doublons non autorisé' });
+		return res.status(401).json({ erreur: e.message });
+	}
+});
+/**==========================================================
+ * PUT
+   ==========================================================*/
+/*
+ * Objet attendu en body : 
+  {
+	"id" : number,
+    "title" : "reqdusire",
+    "description" : "require",
+    "github" : "require",
+    "image_path" : "require",
+    "skills" : "require"
+  }
+*/
+router.put('/update', requireAuth, (req, res) => {
+	const { id, title, description, github, image_path, skills } = req.body;
+	try {
+		if (!id || !title || !description || !github || !image_path || !skills) throw new Error('champ vide ou mauvais objet envoyé');
+		if (!isValidNumber(id) || !isValidString(title) || !isValidString(description) || !isValidString(github) || !isValidString(image_path) || !isValidString(skills)) throw new Error('champ vide ou mauvais objet envoyé');
+
+		const rows = db.prepare(`update projects set title_projects = ?, description_projects = ?, github_projects = ?,  image_path_projects = ?, skills_projects = ? WHERE id_projects = ? RETURNING *;`).get(title, description, github, image_path, skills, id);
+
+		if (!rows) throw new Error(noDataInBDD);
+
+		const data = getData(rows);
+		return res.status(200).json(data);
+	} catch (e) {
+		if (e.code === uniqueConstraint) return res.status(401).json({ erreur: uniqueConstraint });
+		if (e.code === noDataInBDD) return res.status(401).json({ erreur: noDataInBDD });
+		return res.status(401).json({ erreur: e.message });
+	}
+});
+/**
+ * DELETE
+ */
 export default router;
--- controllers/skills.routes.js
+++ controllers/skills.routes.js
@@ -3,14 +3,129 @@ import db from '../db/database.js';
 import requireAuth from '../middleware/auth.js';
 
 const router = Router();
+const uniqueConstraint = `SQLITE_CONSTRAINT_UNIQUE`;
+const noDataInBDD = `No data in bdd`;
 
-router.get('/all', (req, res) => {
-  try {
-    const rows = db.prepare(`SELECT * from skills_view`).all();
+const isValidString = (s) => {
+	return typeof s === 'string' && s.trim().length > 0;
+};
+const isValidNumber = (n) => {
+	return Number.isInteger(n) && n > 0;
+};
 
-    res.json(rows);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+/**
+ * Modifie la structure de l'objet renvoyé pour ne pas matcher avec la bdd
+ * @param {*} rows
+ * @returns
+ */
+const getData = (rows) => {
+	return {
+		id: rows.id_skills,
+		name: rows.name_skills,
+		description: rows.description_skills,
+		image_path: rows.image_path_skills,
+	};
+};
+/**==========================================================
+ * GET
+   ==========================================================*/
+router.get('/all', requireAuth, (req, res) => {
+	try {
+		const rows = db.prepare(`SELECT * from skills_view`).all();
+
+		res.json(rows);
+	} catch (err) {
+		return res.status(500).json({ error: err.message });
+	}
+});
+
+/**==========================================================
+ * POST
+   ==========================================================*/
+
+/*
+ * Objet attendu en body : 
+  {
+    "name" : "require",
+    "image_path" : "require",
+    "description" : "require"
   }
+*/
+router.post('/create', requireAuth, (req, res) => {
+	const { name, image_path, description } = req.body;
+	try {
+		if (!name || !image_path || !description) throw new Error('champ vide ou mauvais objet envoyé');
+		if (!isValidString(name) || !isValidString(image_path) || !isValidString(description)) throw new Error('champ vide ou mauvais objet envoyé');
+		//.get et pas .run car on retourne tout l'objet inséré
+		const rows = db.prepare(`INSERT INTO skills (name_skills, image_path_skills, description_skills) VALUES (?,?,?) RETURNING *;`).get(name, image_path, description);
+
+		const data = getData(rows);
+
+		return res.status(200).json(data);
+	} catch (e) {
+		if (e.code === uniqueConstraint) return res.status(401).json({ erreur: 'Doublons non autorisé' });
+		return res.status(401).json({ erreur: e.message });
+	}
+});
+
+/**==========================================================
+ * PUT
+   ==========================================================*/
+/*
+ * Objet attendu en body : 
+  {
+    "id" : number,
+    "name" : "require",
+    "image_path" : "require",
+    "description" : "require"
+  }
+*/
+router.put('/update', requireAuth, (req, res) => {
+	const { id, name, image_path, description } = req.body;
+	try {
+		if (!id || !name || !image_path || !description) throw new Error('champ vide ou mauvais objet envoyé');
+		if (!isValidNumber(id) || !isValidString(name) || !isValidString(image_path) || !isValidString(description)) throw new Error('champ vide ou mauvais objet envoyé');
+
+		const rows = db.prepare(`update skills set name_skills = ?, image_path_skills = ?, description_skills = ? WHERE id_skills = ? RETURNING *;`).get(name, image_path, description, id);
+
+		if (!rows) throw new Error(noDataInBDD);
+
+		const data = getData(rows);
+		return res.status(200).json(data);
+	} catch (e) {
+		if (e.code === uniqueConstraint) return res.status(401).json({ erreur: uniqueConstraint });
+		if (e.code === noDataInBDD) return res.status(401).json({ erreur: noDataInBDD });
+		return res.status(401).json({ erreur: e.message });
+	}
+});
+/**==========================================================
+ * DELETE
+   ==========================================================*/
+/**
+ * Objet attendu :
+/*
+ * Objet attendu en body : 
+  {
+    "id" : number
+  }
+*/
+
+router.delete('/delete', requireAuth, (req, res) => {
+	const { id } = req.body;
+	try {
+		if (!id) throw new Error('champ vide ou mauvais objet envoyé');
+		if (!isValidNumber(id)) throw new Error("L'id n'est pas un nombre valide");
+
+		const rows = db.prepare(`DELETE FROM skills WHERE id_skills = ? RETURNING *;`).get(id);
+
+		if (!rows) throw new Error(noDataInBDD);
+
+		const data = getData(rows);
+		return res.status(200).json({ delete: data });
+	} catch (e) {
+		if (e.code === uniqueConstraint) return res.status(401).json({ erreur: uniqueConstraint });
+		if (e.code === noDataInBDD) return res.status(401).json({ erreur: noDataInBDD });
+		return res.status(401).json({ erreur: e.message });
+	}
 });
 export default router;
